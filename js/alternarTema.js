fafb8bb58d294b5f9f024e4121a86798e397ae25
Ciro Dourado <ciro.brz@gmail.com>
2021-09-01 15:55:21 -0300

    separando temas entre claro e escuro

---
+++ js/alternarTema.js
@@ -0,0 +1,33 @@
+const tema = document.querySelector("link#temaGlobal");
+
+
+function estaNoIntervaloDaNoite() {
+    let atual = (new Date()).getHours();
+    let noite     =  atual >= 17.5;
+    let madrugada = (atual >=    0 && atual <=  5.5);
+    return noite || madrugada;
+}
+
+function verificarHoras() {
+    switch ( estaNoIntervaloDaNoite() ) {
+        case true: trocarParaTemaEscuro(); break;
+        default  : trocarParaTemaClaro();
+    }
+} // verificarHoras
+verificarHoras();
+
+let aCadaHora = 3600000;
+setInterval(verificarHoras, aCadaHora);
+
+
+function trocarParaTemaEscuro() {
+    if (tema.getAttribute("href") == "css/temaClaro.css") {
+        tema.href = "css/temaEscuro.css";
+    }
+} // trocarParaTemaEscuro
+
+function trocarParaTemaClaro() {
+    if (tema.getAttribute("href") == "css/temaEscuro.css") {
+        tema.href = "css/temaClaro.css";
+    }
+} // trocarParaTemaClaro
