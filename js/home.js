322e2a90f43f17e87c46cb23c916676afcc4bc75
Ciro Dourado <ciro.brz@gmail.com>
2021-09-07 17:28:35 -0300

    top secret

--- js/home.js
+++ js/home.js
@@ -1,24 +1,31 @@
 console.log(
     'Olá criatura curiosa kk :3\n' +
-    'Você acabou de se deparar com um easter egg!!\n' +
-    'se você, desenvolvedor, se achar à altura, ' +
+    'Você acabou de se deparar com um easter egg!!\n\n' +
+    'Se você, desenvolvedor, se achar à altura, ' +
     'resolva este desafio:'
 )
 console.log(
-    'Digamos que temos um conjunto de strings que representam ' +
+    '    Digamos que temos um conjunto de strings que representam ' +
     'os primeiros nomes dos habitantes de uma pequena cidade.\n' +
-    'Esta cidade possui no total 50.000 habitantes.\n' +
-    'O nosso problema aqui é: quantos desses 50.000 nomes ' +
+    '    Esta cidade possui no total 50.000 habitantes.\n' +
+    '    O nosso problema aqui é: quantos desses 50.000 nomes ' +
     'armazenados fazem parte de uma lista menor com 500 ' +
     'nomes únicos?'
 )
 console.log(
-    'Seja n o número de elementos da lista maior.\n' +
-    'A complexidade não pode ultrapassar O(2n); \n' +
-    'O espaço não pode ultrapassar O(n); \n' +
-    'A lista maior é imutável, ' +
+    'Seja n o número de elementos da lista maior:\n' +
+    '|> A complexidade não pode ultrapassar O(2n); \n' +
+    '|> O espaço não pode ultrapassar O(n); \n' +
+    '|> A lista maior é imutável, ' +
     'não se encontra ordenada, ' +
-    'e pode ter elementos repetidos'
+    'e pode ter elementos repetidos.'
+)
+console.log(
+	'O código pode ser feito na linguagem de sua escolha, ' +
+	'ou, se preferir, explique-o como algoritmo de forma clara.\n\n' +
+	'Sinta-se à vontade para enviar ao meu e-mail ' +
+	'(com o assunto "Desafio / Easter Egg"): \n' +
+	'ciro dot brz at gmail dot com'
 )
 
 let nav     = document.getElementsByTagName('nav')[0]
