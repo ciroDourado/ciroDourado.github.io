809ca2368c8f79b45858be21f1c3e1710501011c
Ciro Dourado <ciro.brz@gmail.com>
2021-09-07 13:35:40 -0300

    top secret

--- js/home.js
+++ js/home.js
@@ -1,28 +1,24 @@
-console.log('olá criatura curiosa kk :3')
-console.log('você acabou de se deparar com um easter egg')
 console.log(
-    'se você, desenvolvedor, se achar à altura, \
-    resolva este desafio:'
+    'Olá criatura curiosa kk :3\n' +
+    'Você acabou de se deparar com um easter egg!!\n' +
+    'se você, desenvolvedor, se achar à altura, ' +
+    'resolva este desafio:'
 )
 console.log(
-    'Digamos que temos um conjunto de strings que representam \
-    os primeiros nomes dos habitantes de uma pequena cidade'
+    'Digamos que temos um conjunto de strings que representam ' +
+    'os primeiros nomes dos habitantes de uma pequena cidade.\n' +
+    'Esta cidade possui no total 50.000 habitantes.\n' +
+    'O nosso problema aqui é: quantos desses 50.000 nomes ' +
+    'armazenados fazem parte de uma lista menor com 500 ' +
+    'nomes únicos?'
 )
 console.log(
-    'Esta cidade possui no total 50.000 habitantes.'
-)
-console.log(
-    'O nosso problema aqui é: quantos desses 50.000 nomes \
-    armazenados fazem parte de uma lista menor com 500 \
-    nomes únicos?'
-)
-console.log('Seja n o número de elementos da lista maior')
-console.log('A complexidade não pode ultrapassar O(2n)')
-console.log('O espaço não pode ultrapassar O(n)')
-console.log(
-    'A lista maior é imutável, \
-    não se encontra ordenada, \
-    e pode ter elementos repetidos'
+    'Seja n o número de elementos da lista maior.\n' +
+    'A complexidade não pode ultrapassar O(2n); \n' +
+    'O espaço não pode ultrapassar O(n); \n' +
+    'A lista maior é imutável, ' +
+    'não se encontra ordenada, ' +
+    'e pode ter elementos repetidos'
 )
 
 let nav     = document.getElementsByTagName('nav')[0]
